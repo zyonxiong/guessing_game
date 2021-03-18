fdbd785d0924507ff35056b41240450edefe15d8
Zyon Xiong <xiong.houa95@gmail.com>
2021-03-17 21:39:49 -0500

    Added user guess fall restriction range

--- game.py
+++ game.py
@@ -15,14 +15,15 @@ while True:
     # try and except helps handling with error
     try:
         guess = int(input("Your guess? "))
-        if guess > 0:
-            if guess < num:
-                print("Your guess is too low, try again.")
-            elif guess > num:
-                print("Your guess is too high, try again.")
-            else:
-                print("Congrats, you got it right!")
-                break
+        if guess < 0 or guess > 100:
+            print("Nice try but you're out of line, try again.")
+        elif guess < num:
+            print("Your guess is too low, try again.")
+        elif guess > num:
+            print("Your guess is too high, try again.")
+        else:
+            print("Congrats, you got it right!")
+            break
 
     except ValueError:
         print("That's not a valid response. Please try again.")
